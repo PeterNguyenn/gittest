5d8989681f7e9982c469c8a522fd1dc4ff419b36
Peter <thong01677@gmail.com>
2020-02-19 22:34:42 -0500

    First commit of project

---
+++ src/testgit/Testgit.java
@@ -0,0 +1,34 @@
+/*
+    Name:  [your name here]
+    Assignment:  [assignment name]
+    Program: [your program name here]
+    Date:  [assignment due date here]
+    
+    Description:
+    [program description in your own words]
+*/
+
+package testgit;
+
+/**
+ *
+ * @author peter
+ */
+public class Testgit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+	public double gett() {
+		// TODO - implement Testgit.gett
+		throw new UnsupportedOperationException();
+	}
+
+	private String mail;
+	private double lala;
+
+}
---
+++ src/testgit/gitgit.java
@@ -0,0 +1,28 @@
+package testgit;
+
+public class gitgit {
+
+	private String name;
+	private int value;
+	private String phone;
+
+	public gitgit() {
+		// TODO - implement gitgit.gitgit
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param pho
+	 */
+	public void getLo(String pho) {
+		// TODO - implement gitgit.getLo
+		throw new UnsupportedOperationException();
+	}
+
+	public int setl() {
+		// TODO - implement gitgit.setl
+		throw new UnsupportedOperationException();
+	}
+
+}
